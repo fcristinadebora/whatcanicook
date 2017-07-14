d7f0b62b64a1bab208cfb19ddbc8c633787ba7ac
starasenko <STarasenko@relevantmedia.ru>
2017-07-14 14:23:08 +0300

    remove vue-strap

--- pages/index.vue
+++ pages/index.vue
@@ -4,41 +4,21 @@
 
 <template>
   <div class="container">
-    <h1>Fuck</h1>
-    <nav aria-label="Page navigation">
-      <ul class="pagination">
-        <li>
-          <a href="#" aria-label="Previous">
-            <span aria-hidden="true">&laquo;</span>
-          </a>
-        </li>
-        <li>
-          <a href="#">1</a>
-        </li>
-        <li>
-          <a href="#">2</a>
-        </li>
-        <li>
-          <a href="#">3</a>
-        </li>
-        <li>
-          <a href="#">4</a>
-        </li>
-        <li>
-          <a href="#">5</a>
-        </li>
-        <li>
-          <a href="#" aria-label="Next">
-            <span aria-hidden="true">&raquo;</span>
-          </a>
-        </li>
-      </ul>
-    </nav>
+    <h1>Nuxtstrap</h1>
+  
   </div>
 </template>
 
 <script>
-export default {
+import {
+  tabset,
+  tabGroup,
+  tab
+} from 'vue-strap'
 
+export default {
+  components: {
+    tabset
+  }
 }
 </script>
